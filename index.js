57e4451fd54e326e76062dda367c059f4ff6be03
Sumit Amrutkar <56144263+sumit0106@users.noreply.github.com>
2025-07-13 01:11:24 +0530

    Update index.js Double Quote errors

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const prometheus = require('express-prometheus-middleware');
+const prometheus = require("express-prometheus-middleware");
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(prometheus({
-  metricsPath: '/metrics',
+  metricsPath: "/metrics",
   collectDefaultMetrics: true,
 }));
 
-app.post('/telemetry', (req, res) => {
-  console.log('Telemetry Received:', req.body);
-  res.status(200).json({ message: 'Telemetry received' });
+app.post("/telemetry", (req, res) => {
+  console.log("Telemetry Received:", req.body);
+  res.status(200).json({ message: "Telemetry received" });
 });
 
-app.get('/health', (req, res) => {
-  res.send('OK');
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
