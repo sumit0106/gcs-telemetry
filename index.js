e3fee8bba02fa651db9f6897c122d92eeb02dd78
Sumit Amrutkar <56144263+sumit0106@users.noreply.github.com>
2025-07-13 11:57:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get("/metrics", async (req, res) => {
   res.end(await promClient.register.metrics());
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
 
